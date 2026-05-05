ce21ed75f8b73e4d81a790d74580d969b9c6fe0a
abdohemdan <abdohemdan0101@gmail.com>
2026-05-05 13:08:46 +0200

    Switch to PostgreSQL

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -3,15 +3,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-private static final String URL = "jdbc:mysql://b6fqjihuzy40pxap35pl-mysql.services.clever-cloud.com:3306/b6fqjihuzy40pxap35pl?useSSL=true&serverTimezone=UTC&allowPublicKeyRetrieval=true&sslMode=REQUIRED";
-private static final String USER = "uqoskkdbxvsuuuv2";
-private static final String PASS = "2xnhUg4VyNSLrkT6aOpk";
-    
+private static final String URL = "jdbc:postgresql://dpg-d7s8ocn7f7vs73dghaj0-a.frankfurt-postgres.render.com:5432/giocodb";
+private static final String USER = "giocodb_user";
+private static final String PASS = "aUP9VdHcR0yDcuWvL7WZ8RdZWY3bvwum";
+
     public static Connection getConnection() throws SQLException {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
-            throw new SQLException("Driver MySQL non trovato: " + e.getMessage());
+            throw new SQLException("Driver PostgreSQL non trovato: " + e.getMessage());
         }
         return DriverManager.getConnection(URL, USER, PASS);
     }
